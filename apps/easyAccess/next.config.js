70dc51993012436ab2e75ee32e344dc175851a19
丁俊杰 <10725046+JunJD@user.noreply.gitee.com>
2024-09-04 01:44:45 +0800

    feat: add resume list

--- apps/easyAccess/next.config.js
+++ apps/easyAccess/next.config.js
@@ -14,6 +14,9 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  images: {
+    domains: ['ui.shadcn.com'],
+  }
 };
 
 const plugins = [
--- apps/easyAccess/src/app/[locale]/dashboard/resumes/_layouts/grid/index.tsx
+++ apps/easyAccess/src/app/[locale]/dashboard/resumes/_layouts/grid/index.tsx
@@ -5,6 +5,7 @@ import { BaseCard } from "./_components/base-card";
 
 import { ResumeCard } from "./_components/resume-card";
 import { sortByDate } from "@easy-access/utils";
+import Image from "next/image";
 
 export const GridView = () => {
   // const { resumes, loading } = useResumes();
@@ -30,7 +31,19 @@ export const GridView = () => {
             className="duration-300 animate-in fade-in"
             style={{ animationFillMode: "backwards", animationDelay: `${i * 300}ms` }}
           >
-            <BaseCard />
+            <BaseCard >
+              <div className="space-y-3 w-full">
+                <div className="overflow-hidden rounded-md">
+                  <Image width="250" height="330" className="h-auto w-auto object-cover transition-all hover:scale-105 aspect-[3/4]"
+                    src="https://ui.shadcn.com/_next/image?url=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1611348586804-61bf6c080437%3Fw%3D300%26dpr%3D2%26q%3D80&w=640&q=75"
+                    style={{ color: 'transparent' }} alt="img" />
+                </div>
+                <div className="space-y-1 text-sm"><h3 className="font-medium leading-none">React Rendezvous</h3><p className="text-xs text-muted-foreground">
+                  Ethan Byte
+                </p>
+                </div>
+              </div>
+            </BaseCard>
           </div>
         ))}
 
